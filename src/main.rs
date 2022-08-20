3fed56c4bfeafcb5291301a7d3e37eddb7d13444
Alfan Nur Fauzan <alpancs@gmail.com>
2022-08-20 22:40:17 +0700

    init the web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+async fn webhook_telegram() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|x| x.parse().ok())
+        .unwrap_or(8080);
+
+    let telegram_bot_token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN");
+    HttpServer::new(move || {
+        App::new().route(
+            &format!("/webhook/telegram/{}", telegram_bot_token),
+            web::post().to(webhook_telegram),
+        )
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
