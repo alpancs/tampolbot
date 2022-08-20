e154f66821b0870b036b5a3af33f6e2500bf268e
Alfan Nur Fauzan <alpancs@gmail.com>
2022-08-21 00:47:38 +0700

    reply to reply to

--- src/webhook_telegram.rs
+++ src/webhook_telegram.rs
@@ -9,7 +9,9 @@ pub struct Update {
 
 #[derive(Deserialize, Default)]
 struct Message {
+    message_id: i64,
     chat: Chat,
+    reply_to_message: Box<Message>,
     text: String,
 }
 
@@ -23,12 +25,23 @@ struct TelegramResponse {
     method: &'static str,
     chat_id: i64,
     text: String,
+    reply_to_message_id: i64,
 }
 
 pub async fn handle_post(update: web::Json<Update>) -> impl Responder {
-    web::Json(TelegramResponse {
-        method: "sendMessage",
-        chat_id: update.message.chat.id,
-        text: update.message.text.to_string(),
-    })
+    if update.message.reply_to_message.message_id == 0 {
+        web::Json(TelegramResponse {
+            method: "sendMessage",
+            chat_id: update.message.chat.id,
+            text: update.message.text.to_string(),
+            reply_to_message_id: update.message.message_id,
+        })
+    } else {
+        web::Json(TelegramResponse {
+            method: "sendMessage",
+            chat_id: update.message.chat.id,
+            text: update.message.reply_to_message.text.to_string(),
+            reply_to_message_id: update.message.reply_to_message.message_id,
+        })
+    }
 }
